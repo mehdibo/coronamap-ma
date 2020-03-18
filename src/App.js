c96eff835e0fa5f05cf8b714cecc218a69204071
younes boumalek <younes@youness-MacBook-Pro.local>
2020-03-18 02:46:22 +0100

    Add alert Emergency number

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {this.props.statistics}
         <Alert style={{zIndex:99999,position:'absolute',bottom:0,margin:10, fontSize:20,fontWeight: 'bold',color:'#ecf0f1',backgroundColor:'#e74c3c'}} severity="">Numero d'urgence — 0801004747</Alert>
+        {this.props.statistics}
         <Map
           center={[29, -6]}
           zoom={6}
