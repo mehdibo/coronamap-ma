1a15500e77a0d1334f2a1f42cc9bf7f47c4389ee
Mehdi Bounya <mehdi.bounya@gmail.com>
2020-03-18 02:01:48 +0100

    Added link to repo

--- src/App.js
+++ src/App.js
@@ -7,20 +7,27 @@ class App extends React.Component {
       <div>
         {this.props.statistics}
         <Map
-        center={[29, -6]}
-        zoom={6}
-        maxZoom={10}
-        attributionControl={true}
-        zoomControl={true}
-        doubleClickZoom={true}
-        scrollWheelZoom={true}
-        dragging={true}
-        animate={true}
-        easeLinearity={0.35}
+          center={[29, -6]}
+          zoom={6}
+          maxZoom={10}
+          attributionControl={true}
+          zoomControl={true}
+          doubleClickZoom={true}
+          scrollWheelZoom={true}
+          dragging={true}
+          animate={true}
+          easeLinearity={0.35}
         >
           <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
           {this.props.markers}
         </Map>
+        <a
+          href="https://github.com/mehdibo/coronamap-ma"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View source on GitHub
+        </a>
       </div>
     );
   }
