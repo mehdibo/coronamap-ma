31196998fd22024c620de453774d2441bf05f7ad
younes boumalek <younes@youness-MacBook-Pro.local>
2020-03-18 02:12:01 +0100

    Add alert Emergency number

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Map, TileLayer } from "react-leaflet";
+import Alert from '@material-ui/lab/Alert';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         {this.props.statistics}
+        <Alert style={{zIndex:99999,position:'absolute',bottom:0,margin:10, fontSize:20,fontWeight: 'bold',color:'#ecf0f1',backgroundColor:'#e74c3c'}} severity="">Numero d'urgence — 0801004747</Alert>
         <Map
         center={[29, -6]}
         zoom={6}
@@ -21,6 +23,7 @@ class App extends React.Component {
           <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
           {this.props.markers}
         </Map>
+        
       </div>
     );
   }
